9f30b3cb12d4daa87321c3f5f62e843ad2c526d7
Sam <34483710+huyngo233@users.noreply.github.com>
2023-02-08 07:44:04 +0700

    Update videojs-ima-countdown.js

--- videojs-ima-countdown.js
+++ videojs-ima-countdown.js
@@ -1,1 +1,88 @@
-will update later
+/*! @name videojs-ima-countdown-nzh @version 0.0.0 @license MIT */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('video.js')) :
+  typeof define === 'function' && define.amd ? define(['video.js'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.videojsImaCountdownNzh = factory(global.videojs));
+}(this, (function (videojs) { 'use strict';
+
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+  var videojs__default = /*#__PURE__*/_interopDefaultLegacy(videojs);
+
+  var version = "0.0.0";
+
+  var defaults = {
+    debug: false,
+    text: 'Advertisement',
+    controlBarPosition: 1
+  }; // class Countdown extends Component {
+
+
+  var registerPlugin = videojs__default['default'].registerPlugin || videojs__default['default'].plugin; // const dom = videojs.dom || videojs;
+
+  /**
+   * Function to invoke when the player is ready.
+   *
+   * This is a great place for your plugin to initialize itself. When this
+   * function is called, the player will have its DOM and child components
+   * in place.
+   *
+   * @function onPlayerReady
+   * @param    {Player} player
+   *           A Video.js player object.
+   *
+   * @param    {Object} [options={}]
+   *           A plain object containing options for the plugin.
+   */
+
+  var onPlayerReady = function onPlayerReady(player, options) {
+    player.addClass('vjs-ima-countdown');
+    var settings = videojs__default['default'].mergeOptions({}, defaults, options || {});
+    settings.timerInterval = null;
+    settings.timeEl = null;
+    settings.timeRemaining = null;
+    player.countdown = settings; // player.on('ads-load', function() {
+    //   onAdLoad(player);
+    // });
+
+    var textDisplay = document.createElement('p');
+    textDisplay.className = 'vjs-text';
+    textDisplay.innerHTML = "Becoming a plugin developer";
+    console.log('playerlocal', player);
+    player.el().appendChild(textDisplay);
+    var controlBar = player.controlBar.el();
+    var time = document.createElement('span');
+    time.className = 'vjs-text';
+    time.innerHTML = "0:00";
+    controlBar.appendChild(time);
+  };
+  /**
+   * A video.js plugin.
+   *
+   * In the plugin function, the value of `this` is a video.js `Player`
+   * instance. You cannot rely on the player being in a "ready" state here,
+   * depending on how the plugin is invoked. This may or may not be important
+   * to you; if not, remove the wait for "ready"!
+   *
+   * @function imaCountdown
+   * @param    {Object} [options={}]
+   *           An object of options left to the plugin author to define.
+   */
+
+
+  var imaCountdown = function imaCountdown(options) {
+    var _this = this;
+
+    this.ready(function () {
+      onPlayerReady(_this, videojs__default['default'].mergeOptions(defaults, options));
+    });
+  }; // Register the plugin with video.js.
+
+
+  registerPlugin('imaCountdown', imaCountdown); // Include the version number.
+
+  imaCountdown.VERSION = version;
+
+  return imaCountdown;
+
+})));
